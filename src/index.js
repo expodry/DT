49a08e38fde33e07a16249fe158352d764d6da42
denskarlet <den.skarlet@gmail.com>
2020-05-09 10:40:04 -0400

    first test push no code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>asdlo, world!</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>buhbye, world!</h1>, document.getElementById('root'));
